67b408ecc0a630b4486eea88cc3990eabd7aaac8
architect-dev <halftonedev@gmail.com>
2025-04-10 15:04:06 +0200

    Test FHERC20 constructor, indicatedBalances, and mint

---
+++ packages/hardhat/test/FHERC20.test.ts
@@ -0,0 +1,123 @@
+import { expect } from "chai";
+import hre, { ethers } from "hardhat";
+import {
+  ConfidentialETH,
+  contracts,
+  ERC20_Harness,
+  FHEContract,
+  FHERC20_Harness,
+  WETH_Harness,
+} from "../typechain-types";
+import { cofhejs, Encryptable, FheTypes } from "cofhejs/node";
+import { RedactCore } from "../typechain-types";
+import { expectFHERC20BalancesChange, prepExpectFHERC20BalancesChange, ticksToIndicated, tick } from "./utils";
+import { ZeroAddress } from "ethers";
+
+describe.only("FHERC20", function () {
+  // We define a fixture to reuse the same setup in every test.
+  const deployContracts = async () => {
+    // Deploy wBTC
+    const XFHEFactory = await ethers.getContractFactory("FHERC20_Harness");
+    const XFHE = (await XFHEFactory.deploy("Unknown FHERC20", "XFHE", 18)) as FHERC20_Harness;
+    await XFHE.waitForDeployment();
+
+    return { XFHE };
+  };
+
+  async function setupFixture() {
+    const [owner, bob, alice] = await ethers.getSigners();
+    const { XFHE } = await deployContracts();
+
+    return { owner, bob, alice, XFHE };
+  }
+
+  describe("initialization", function () {
+    it("Should be constructed correctly", async function () {
+      const { XFHE } = await setupFixture();
+
+      expect(await XFHE.name()).to.equal("Unknown FHERC20");
+      expect(await XFHE.symbol()).to.equal("XFHE");
+      expect(await XFHE.decimals()).to.equal(18);
+      expect(await XFHE.balanceOfIsIndicator()).to.equal(true);
+      expect(await XFHE.indicatorTick()).to.equal(10 ** (18 - 4));
+      expect(await XFHE.isFherc20()).to.equal(true);
+    });
+  });
+
+  describe("indicated balances", function () {
+    it("indicated balances should wrap around", async function () {
+      const { bob, XFHE } = await setupFixture();
+
+      const mintValue = ethers.parseEther("10");
+      const burnValue = ethers.parseEther("1");
+
+      // Balance 9999 -> wraparound -> 5001
+      await XFHE.setUserIndicatedBalance(bob, 9999);
+      await XFHE.mint(bob, mintValue);
+      expect(await XFHE.balanceOf(bob)).to.equal(await ticksToIndicated(XFHE, 5001n));
+
+      // Balance 1 -> wraparound -> 4999
+      await XFHE.setUserIndicatedBalance(bob, 1);
+      await XFHE.burn(bob, burnValue);
+      expect(await XFHE.balanceOf(bob)).to.equal(await ticksToIndicated(XFHE, 4999n));
+
+      // Total supply 9999 -> wraparound -> 5001
+      await XFHE.setTotalIndicatedSupply(9999);
+      await XFHE.mint(bob, mintValue);
+      expect(await XFHE.totalSupply()).to.equal(await ticksToIndicated(XFHE, 5001n));
+
+      // Total supply 1 -> wraparound -> 4999
+      await XFHE.setTotalIndicatedSupply(1);
+      await XFHE.burn(bob, burnValue);
+      expect(await XFHE.totalSupply()).to.equal(await ticksToIndicated(XFHE, 4999n));
+    });
+  });
+
+  describe("mint", function () {
+    it("should mint", async function () {
+      const { bob, XFHE } = await setupFixture();
+
+      expect(await XFHE.totalSupply()).to.equal(0);
+      expect(await ticksToIndicated(XFHE, 0n)).to.equal(0n);
+      expect(await XFHE.encTotalSupply()).to.equal(0n, "Total supply not initialized (hash is 0)");
+
+      // 1st TX, indicated + 5001, true + 1e18
+
+      const value = ethers.parseEther("1");
+
+      await prepExpectFHERC20BalancesChange(XFHE, bob.address);
+
+      await expect(XFHE.mint(bob.address, value))
+        .to.emit(XFHE, "Transfer")
+        .withArgs(ZeroAddress, bob.address, await tick(XFHE));
+
+      await expectFHERC20BalancesChange(XFHE, bob.address, await ticksToIndicated(XFHE, 5001n), value);
+
+      expect(await XFHE.totalSupply()).to.equal(
+        await ticksToIndicated(XFHE, 5001n),
+        "Total indicated supply increases",
+      );
+      await hre.cofhe.mocks.expectPlaintext(await XFHE.encTotalSupply(), value);
+
+      // 2nd TX, indicated + 1, true + 1e18
+
+      await prepExpectFHERC20BalancesChange(XFHE, bob.address);
+
+      await hre.cofhe.mocks.withLogs("XFHE.mint()", async () => {
+        await expect(XFHE.mint(bob.address, value))
+          .to.emit(XFHE, "Transfer")
+          .withArgs(ZeroAddress, bob.address, await tick(XFHE));
+      });
+
+      await expectFHERC20BalancesChange(XFHE, bob.address, await tick(XFHE), value);
+    });
+    it("Should revert if minting to the zero address", async function () {
+      const { XFHE } = await setupFixture();
+
+      await expect(XFHE.mint(ZeroAddress, ethers.parseEther("1"))).to.be.revertedWithCustomError(
+        XFHE,
+        "ERC20InvalidReceiver",
+      );
+    });
+  });
+});
---
+++ packages/hardhat/test/utils.ts
@@ -0,0 +1,68 @@
+import { expect } from "chai";
+import { FHERC20 } from "../typechain-types";
+import hre from "hardhat";
+
+export const ticksToIndicated = async (token: FHERC20, ticks: bigint): Promise<bigint> => {
+  const tick = await token.indicatorTick();
+  return ticks * BigInt(tick);
+};
+
+export const tick = async (token: FHERC20): Promise<bigint> => {
+  return token.indicatorTick();
+};
+
+const indicatedBalances = new Map<string, bigint>();
+const encBalances = new Map<string, bigint>();
+
+export const prepExpectFHERC20BalancesChange = async (token: FHERC20, account: string) => {
+  indicatedBalances.set(account, await token.balanceOf(account));
+  const encBalanceHash = await token.encBalanceOf(account);
+  const encBalance = await hre.cofhe.mocks.getPlaintext(encBalanceHash);
+  console.log({ encBalanceHash, encBalance });
+  encBalances.set(account, encBalance);
+};
+
+export const expectFHERC20BalancesChange = async (
+  token: FHERC20,
+  account: string,
+  expectedIndicatedChange: bigint,
+  expectedEncChange: bigint,
+) => {
+  const symbol = await token.symbol();
+
+  const currIndicated = await token.balanceOf(account);
+  const prevIndicated = indicatedBalances.get(account)!;
+  const indicatedChange = currIndicated - prevIndicated;
+  expect(indicatedChange).to.equal(
+    expectedIndicatedChange,
+    `${symbol} (FHERC20) indicated balance change for ${account} is incorrect. Expected: ${expectedIndicatedChange}, received: ${indicatedChange}`,
+  );
+
+  const currEncBalanceHash = await token.encBalanceOf(account);
+  const currEncBalance = await hre.cofhe.mocks.getPlaintext(currEncBalanceHash);
+  const prevEncBalance = encBalances.get(account)!;
+  const encChange = currEncBalance - prevEncBalance;
+  console.log({ currEncBalance, prevEncBalance, encChange });
+  expect(encChange).to.equal(
+    expectedEncChange,
+    `${symbol} (FHERC20) encrypted balance change for ${account} is incorrect. Expected: ${expectedEncChange}, received: ${encChange}`,
+  );
+};
+
+const erc20Balances = new Map<string, bigint>();
+
+export const prepExpectERC20BalancesChange = async (token: FHERC20, account: string) => {
+  erc20Balances.set(account, await token.balanceOf(account));
+};
+
+export const expectERC20BalancesChange = async (token: FHERC20, account: string, expectedChange: bigint) => {
+  const symbol = await token.symbol();
+
+  const currBal = await token.balanceOf(account);
+  const prevBal = erc20Balances.get(account)!;
+  const delta = currBal - prevBal;
+  expect(delta).to.equal(
+    expectedChange,
+    `${symbol} (ERC20) balance change for ${account} is incorrect. Expected: ${expectedChange}, received: ${delta}`,
+  );
+};
