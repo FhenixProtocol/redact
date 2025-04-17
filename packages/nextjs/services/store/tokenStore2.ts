b39eb41d82c668bad82fc1e04004fc70799d3a67
architect-dev <halftonedev@gmail.com>
2025-04-17 17:54:05 +0200

    Create reusable superjson storage. Use in decryption and token stores. Fix useDecryptValue to always return correct object

--- packages/nextjs/services/store/tokenStore2.ts
+++ packages/nextjs/services/store/tokenStore2.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useCallback, useRef } from "react";
 import { wagmiConfig } from "../web3/wagmiConfig";
+import { superjsonStorage } from "./superjsonStorage";
 import { WritableDraft } from "immer";
-import superjson from "superjson";
 import { Address, erc20Abi, zeroAddress } from "viem";
 import { deepEqual, useChainId } from "wagmi";
 import { getAccount, getPublicClient } from "wagmi/actions";
@@ -73,17 +73,7 @@ export const useTokenStore = create<TokenStore>()(
     })),
     {
       name: "token-store",
-      storage: {
-        getItem: name => {
-          const str = localStorage.getItem(name);
-          if (!str) return null;
-          return superjson.parse(str) as unknown as ReturnType<typeof JSON.parse>;
-        },
-        setItem: (name, value) => {
-          localStorage.setItem(name, superjson.stringify(value));
-        },
-        removeItem: name => localStorage.removeItem(name),
-      },
+      storage: superjsonStorage,
     },
   ),
 );
