a14a8d180eeb27655ffab270436077bf5d51a9f2
architect-dev <halftonedev@gmail.com>
2025-04-18 16:07:38 +0200

    Deploy additional test ERC20s

---
+++ packages/hardhat/deploy/03_1_deploy_usdc.ts
@@ -0,0 +1,44 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
+
+const deployErc20s: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  await deploy("wBTC", {
+    contract: "ERC20_Harness",
+    from: deployer,
+    args: ["wBTC", "wBTC", 8],
+    log: true,
+    autoMine: true,
+  });
+
+  const wbtc = await hre.ethers.getContract<Contract>("wBTC", deployer);
+  console.log("wBTC deployed at:", wbtc.target);
+
+  await deploy("MATIC", {
+    contract: "ERC20_Harness",
+    from: deployer,
+    args: ["MATIC", "MATIC", 18],
+    log: true,
+    autoMine: true,
+  });
+
+  const matic = await hre.ethers.getContract<Contract>("MATIC", deployer);
+  console.log("MATIC deployed at:", matic.target);
+
+  await deploy("FTM", {
+    contract: "ERC20_Harness",
+    from: deployer,
+    args: ["FTM", "FTM", 18],
+    log: true,
+    autoMine: true,
+  });
+
+  const ftm = await hre.ethers.getContract<Contract>("FTM", deployer);
+  console.log("FTM deployed at:", ftm.target);
+};
+
+export default deployErc20s;
+deployErc20s.tags = ["erc20s"];
--- packages/hardhat/tasks/hh-prepare-wallet.ts
+++ packages/hardhat/tasks/hh-prepare-wallet.ts
@@ -1,8 +1,27 @@
+import { Address } from "hardhat-deploy/types";
 import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const transferErc20 = async (hre: HardhatRuntimeEnvironment, contractName: string, to: Address, amount: bigint) => {
+  console.log(`Sending ${amount} ${contractName} to ${to}`);
+
+  const { ethers, deployments } = hre;
+  const [sender] = await ethers.getSigners();
+  const contract = await deployments.get(contractName);
+  const contractInstance = new ethers.Contract(contract.address, contract.abi, sender);
+  const decimals = await contractInstance.decimals();
+  const tx = await contractInstance.mint(to, ethers.parseUnits(amount.toString(), decimals));
+  await tx.wait();
+
+  console.log(`Sent ${amount} ${contractName} to ${to}`);
+  console.log(`${contractName} address`, contract.address);
+};
 
 task("hh-prepare-wallet", "Send ETH to a test account")
   .addParam("to", "Recipient address")
-  .setAction(async ({ to }, { ethers, deployments }) => {
+  .setAction(async ({ to }, hre) => {
+    const { ethers } = hre;
+
     console.log("Sending 10 ETH to", to);
 
     const [sender] = await ethers.getSigners();
@@ -15,21 +34,10 @@ task("hh-prepare-wallet", "Send ETH to a test account")
 
     console.log("");
 
-    console.log("Sending 10 USDC to", to);
-    const usdc = await deployments.get("USDC");
-    const usdcContract = new ethers.Contract(usdc.address, usdc.abi, sender);
-    const tx2 = await usdcContract.mint(to, ethers.parseUnits("10", 6));
-    await tx2.wait();
-    console.log(`Sent 10 USDC to ${to}`);
-    console.log("USDC address", usdc.address);
+    const erc20s = ["USDC", "wBTC", "MATIC", "FTM"];
 
-    console.log("");
-
-    console.log("Sending 10 wETH to", to);
-    const weth = await deployments.get("wETH");
-    const wethContract = new ethers.Contract(weth.address, weth.abi, sender);
-    const tx3 = await wethContract.mint(to, ethers.parseUnits("10", 18));
-    await tx3.wait();
-    console.log(`Sent 10 wETH to ${to}`);
-    console.log("wETH address", weth.address);
+    for (const contractName of erc20s) {
+      await transferErc20(hre, contractName, to, 10n);
+      console.log("");
+    }
   });
