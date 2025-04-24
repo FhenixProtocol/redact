462909b4b8adcfc0132ace0c7f694519cce28eb2
Tovi Levis <tovi@fhenix.io>
2025-04-24 12:42:01 +0300

    Fix eslint errors in TransactionHistory

--- packages/nextjs/components/TransactionHistory.tsx
+++ packages/nextjs/components/TransactionHistory.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import { HashLink } from "./HashLink";
 import { formatDistanceToNow } from "date-fns";
 import { formatUnits } from "viem";
 import { useChainId } from "wagmi";
 import { ConfidentialTokenPair, useConfidentialTokenPair } from "~~/services/store/tokenStore";
 import {
+  RedactTransaction,
   TransactionStatus,
   actionToString,
   statusToString,
@@ -14,6 +16,58 @@ interface TransactionHistoryProps {
   pair?: ConfidentialTokenPair;
 }
 
+const TransactionItem = ({ tx }: { tx: RedactTransaction }) => {
+  const tokenPair = useConfidentialTokenPair(tx.tokenAddress);
+  const decimals = tokenPair?.publicToken.decimals ?? 18; // fallback to 18 if not found
+
+  return (
+    <div key={tx.hash} className="bg-base-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow">
+      <div className="flex h-13 gap-4">
+        <div className="text-green-500 text-xl flex items-center">
+          <Image
+            src={`/icons/${actionToString(tx.actionType)}.svg`}
+            alt={actionToString(tx.actionType)}
+            width={32}
+            height={32}
+          />
+        </div>
+
+        <div className="flex flex-col flex-grow justify-between items-stretch">
+          <div className="text-md font-semibold text-primary self-start mb-auto p-0 m-0">
+            {actionToString(tx.actionType)}
+          </div>
+          <div
+            className={`text-sm font-semibold self-start mt-auto ${
+              tx.status === TransactionStatus.Pending
+                ? "text-yellow-800"
+                : tx.status === TransactionStatus.Confirmed
+                  ? "text-success-500"
+                  : "text-error"
+            }`}
+          >
+            {statusToString(tx.status)}
+          </div>
+          <div className="text-xs text-gray-500">{formatDistanceToNow(tx.timestamp, { addSuffix: true })}</div>
+        </div>
+
+        <div className="flex flex-col justify-between items-stretch">
+          <div className="text-md font-semibold text-primary self-end">
+            {formatUnits(tx.tokenAmount, decimals)} {tx.tokenSymbol}
+          </div>
+          <HashLink
+            className="text-xs text-gray-500"
+            buttonSize={3}
+            copyStrokeWidth={1.0}
+            type="token"
+            hash={tx.hash}
+            copyable
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const TransactionHistory = ({ pair }: TransactionHistoryProps) => {
   const chainId = useChainId();
   const transactionsStore = useTransactionStore();
@@ -22,9 +76,6 @@ export const TransactionHistory = ({ pair }: TransactionHistoryProps) => {
     pair !== undefined
       ? transactionsStore.getAllTransactionsByToken(chainId, pair.publicToken.address)
       : transactionsStore.getAllTransactions(chainId);
-
-  // Create a Map to store token address => decimals mapping
-  const decimalsSet: { [key: string]: number } = {};
 
   if (!transactions.length) {
     return (
@@ -38,60 +89,7 @@ export const TransactionHistory = ({ pair }: TransactionHistoryProps) => {
     <div className="flex-grow overflow-x-hidden overflow-y-auto w-full max-w-4xl mx-auto styled-scrollbar">
       <div className="space-y-4">
         {transactions.map(tx => {
-          // Get or set decimals for this token
-          // Or we can just add the decimals to the transaction object
-          let decimals = decimalsSet[tx.tokenAddress];
-          if (decimals === undefined) {
-            const tokenPair = useConfidentialTokenPair(tx.tokenAddress);
-            decimals = tokenPair?.publicToken.decimals ?? 18; // fallback to 18 if not found
-            decimalsSet[tx.tokenAddress] = decimals;
-          }
-
-          return (
-            <div key={tx.hash} className="bg-base-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow">
-              <div className="flex h-13 gap-4">
-                <div className="text-green-500 text-xl flex items-center">
-                  <img
-                    src={`/icons/${actionToString(tx.actionType)}.svg`}
-                    alt={actionToString(tx.actionType)}
-                    className="w-8 h-8"
-                  />
-                </div>
-
-                <div className="flex flex-col flex-grow justify-between items-stretch">
-                  <div className="text-md font-semibold text-primary self-start mb-auto p-0 m-0">
-                    {actionToString(tx.actionType)}
-                  </div>
-                  <div
-                    className={`text-sm font-semibold self-start mt-auto ${
-                      tx.status === TransactionStatus.Pending
-                        ? "text-yellow-800"
-                        : tx.status === TransactionStatus.Confirmed
-                          ? "text-success-500"
-                          : "text-error"
-                    }`}
-                  >
-                    {statusToString(tx.status)}
-                  </div>
-                  <div className="text-xs text-gray-500">{formatDistanceToNow(tx.timestamp, { addSuffix: true })}</div>
-                </div>
-
-                <div className="flex flex-col justify-between items-stretch">
-                  <div className="text-md font-semibold text-primary self-end">
-                    {formatUnits(tx.tokenAmount, decimals)} {tx.tokenSymbol}
-                  </div>
-                  <HashLink
-                    className="text-xs text-gray-500"
-                    buttonSize={3}
-                    copyStrokeWidth={1.0}
-                    type="token"
-                    hash={tx.hash}
-                    copyable
-                  />
-                </div>
-              </div>
-            </div>
-          );
+          return <TransactionItem key={tx.hash} tx={tx} />;
         })}
       </div>
     </div>
