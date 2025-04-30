f8bf19b9dd5c518e4389b7ad4bd63d88d86363c6
architect-dev <halftonedev@gmail.com>
2025-04-30 12:21:48 +0200

    Update to the in-built notification system. Add tracking txs to transactor.

--- packages/nextjs/utils/scaffold-eth/notification.tsx
+++ packages/nextjs/utils/scaffold-eth/notification.tsx
@@ -1,12 +1,9 @@
 import React from "react";
+import { CircleAlert, CircleCheck, LoaderCircle, TriangleAlert } from "lucide-react";
 import { ToastPosition, toast } from "react-hot-toast";
 import { XMarkIcon } from "@heroicons/react/20/solid";
-import {
-  CheckCircleIcon,
-  ExclamationCircleIcon,
-  ExclamationTriangleIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/24/solid";
+import { HashLink } from "~~/components/HashLink";
+import { cn } from "~~/lib/utils";
 
 type NotificationProps = {
   content: React.ReactNode;
@@ -14,23 +11,25 @@ type NotificationProps = {
   duration?: number;
   icon?: string;
   position?: ToastPosition;
+  txHash?: string;
 };
 
 type NotificationOptions = {
   duration?: number;
   icon?: string;
   position?: ToastPosition;
+  txHash?: string;
 };
 
 const ENUM_STATUSES = {
-  success: <CheckCircleIcon className="w-7 text-success" />,
-  loading: <span className="w-6 loading loading-spinner"></span>,
-  error: <ExclamationCircleIcon className="w-7 text-error" />,
-  info: <InformationCircleIcon className="w-7 text-info" />,
-  warning: <ExclamationTriangleIcon className="w-7 text-warning" />,
+  success: <CircleCheck className="w-5 h-5 text-success-500" />,
+  loading: <LoaderCircle className="w-5 h-5 animate-spin text-primary-accent" />,
+  error: <TriangleAlert className="w-5 h-5 text-destructive" />,
+  info: <CircleAlert className="w-5 h-5 text-primary-accent" />,
+  warning: <TriangleAlert className="w-5 h-5 text-warning-500" />,
 };
 
-const DEFAULT_DURATION = 3000;
+const DEFAULT_DURATION = 4000;
 const DEFAULT_POSITION: ToastPosition = "top-center";
 
 /**
@@ -38,25 +37,36 @@ const DEFAULT_POSITION: ToastPosition = "top-center";
  */
 const Notification = ({
   content,
+  txHash,
   status,
   duration = DEFAULT_DURATION,
   icon,
   position = DEFAULT_POSITION,
 }: NotificationProps) => {
+  console.log("status", status);
   return toast.custom(
     (t: any) => (
       <div
-        className={`flex flex-row items-start justify-between max-w-sm rounded-xl shadow-center shadow-accent bg-base-200 p-4 transform-gpu relative transition-all duration-500 ease-in-out space-x-2
-        ${
+        className={cn(
+          `flex flex-row items-center justify-between rounded-xl shadow-center shadow-accent p-4 gap-4 bg-primary-foreground transform-gpu relative transition-all duration-500 ease-in-out border-2`,
+          status === "success"
+            ? "border-success-500"
+            : status === "error"
+              ? "border-destructive"
+              : status === "info" || status === "loading"
+                ? "border-primary-accent"
+                : "border-background",
           position.substring(0, 3) == "top"
             ? `hover:translate-y-1 ${t.visible ? "top-0" : "-top-96"}`
-            : `hover:-translate-y-1 ${t.visible ? "bottom-0" : "-bottom-96"}`
-        }`}
+            : `hover:-translate-y-1 ${t.visible ? "bottom-0" : "-bottom-96"}`,
+        )}
       >
         <div className="leading-[0] self-center">{icon ? icon : ENUM_STATUSES[status]}</div>
-        <div className={`overflow-x-hidden break-words whitespace-pre-line ${icon ? "mt-1" : ""}`}>{content}</div>
-
-        <div className={`cursor-pointer text-lg ${icon ? "mt-1" : ""}`} onClick={() => toast.dismiss(t.id)}>
+        <div className="flex flex-col">
+          <div className={`break-words whitespace-pre-line text-primary`}>{content}</div>
+          {txHash && <HashLink type="tx" hash={txHash} />}
+        </div>
+        <div className={`cursor-pointer text-lg`} onClick={() => toast.dismiss(t.id)}>
           <XMarkIcon className="w-6 cursor-pointer" onClick={() => toast.remove(t.id)} />
         </div>
       </div>
